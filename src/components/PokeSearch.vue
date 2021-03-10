a155c3e3af6a963c461922752deda16771af0ca4
jorge <jmnch0@gmail.com>
2021-03-10 17:19:29 -0500

    se agrego propiedad de v-model a pokesearch

--- src/components/PokeSearch.vue
+++ src/components/PokeSearch.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="input-container expanded">
     <icon icon="search"></icon>
-    <input type="text" :placeholder="placeholder">
+    <input type="text" v-model="newValue" :placeholder="placeholder" />
   </div>
 </template>
 
@@ -10,12 +10,25 @@ export default {
   name: 'PokeSearch',
   props: {
     placeholder: {
-      type: String
-    }
-  }
-}
+      type: String,
+    },
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      newValue: this.value,
+    };
+  },
+  watch: {
+    value(newValue) {
+      this.newValue = newValue;
+    },
+    newValue(newValue) {
+      this.$emit('input', newValue);
+    },
+  },
+};
 </script>
-
-<style>
-
-</style>
